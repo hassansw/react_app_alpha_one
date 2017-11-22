1f49bc938a354ae9cc24e04c1584257be5ee8bbf
Hassan Saleem <hassan_saleem@outlook.com>
2017-11-23 00:42:34 +0500

    First Commit

---
+++ App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import {Fetch} from 'react-native';
+import { Container, Header, Content, Footer, FooterTab, Button, Icon, Text } from 'native-base';
+
+
+
+export default class FooterTabsIconTextExample extends Component {
+
+  constructor() {
+    super()
+  }
+
+  getMoviesFromApiAsync() {
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        return responseJson.movies;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    return (
+      <Container>
+        <Header />
+        <Content />
+        <Footer>
+          <FooterTab>
+            <Button vertical>
+              <Icon name="apps" />
+              <Text>Apps</Text>
+            </Button>
+            <Button vertical>
+              <Icon name="camera" />
+              <Text>Camera</Text>
+            </Button>
+            <Button vertical active>
+              <Icon active name="navigate" />
+              <Text>Navigate</Text>
+            </Button>
+            <Button vertical>
+              <Icon name="person" />
+              <Text>Contact</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
+}
